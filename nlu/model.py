314ab2b1c73d57e904bf61a9ba87fe14d09ce010
dreymond1 <andrey.alves9@gmail.com>
2021-09-14 15:51:53 -0300

    Adicionando um model

--- nlu/model.py
+++ nlu/model.py
@@ -1,5 +1,5 @@
-import numpy as np
 import yaml
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Embedding
@@ -11,55 +11,53 @@ inputs, outputs = [], []
 
 for command in data['commands']:
     inputs.append(command['input'].lower())
-    outputs.append('{}\{}'.format(command['entity'], command['action']))
+    outputs.append('{}|{}'.format(command['entity'], command['action']))
 
-# Processar texto
-chars = set()
 
-for ch in inputs + outputs:
-    if ch not in chars:
-        chars.add(ch)
+# Processar texto: palavras, caracteres, bytes, sub-palavras
 
-# Mapear caracter-index
-chr2idx = {}
-idx2chr = {}
+max_seq = max([len(bytes(x.encode('utf-8'))) for x in inputs])
 
-for i, ch in enumerate(chars):
-    chr2idx[ch] = i
-    idx2chr[i] = ch
+print('Maior seq:', max_seq)
 
-max_seq = max([len(x) for x in inputs])
+# Criar dataset one-hot (número de examplos, tamanho da seq, num caracteres)
+# Criar dataset disperso (número de examplos, tamanho da seq)
 
-print('Numero de caracteres: ', len(chars))
-print('Maior seq: ', max_seq)
+# Input Data one-hot encoding
 
-# Criar o dataset com one-hot-encoding
+input_data = np.zeros((len(inputs), max_seq, 256), dtype='float32')
+for i, inp in enumerate(inputs):
+    for k, ch in enumerate(bytes(inp.encode('utf-8'))):
+        input_data[i, k, int(ch)] = 1.0
+
+
+# Input data sparse
+'''
 input_data = np.zeros((len(inputs), max_seq), dtype='int32')
 for i, input in enumerate(inputs):
-    for k, ch in enumerate(inputs):
-        input_data[i, k, chr2idx[ch]] = 1.0
-
-# Input_data
-
-input_data = np.zeros((len(inputs), max_seq), dtype='int32')
-
-for i, input in enumerate(inputs):
-    for k, ch in enumerate(inputs):
+    for k, ch in enumerate(input):
         input_data[i, k] = chr2idx[ch]
+'''
 
-# Output_data
+# Output Data
+
 labels = set(outputs)
-labels2idx = {}
-idx2labels = {}
+
+fwrite = open('labels.txt', 'w', encoding='utf-8')
+
+label2idx = {}
+idx2label = {}
 
 for k, label in enumerate(labels):
-    labels2idx[label] = k
-    idx2labels[k] = label
+    label2idx[label] = k
+    idx2label[k] = label
+    fwrite.write(label + '\n')
+fwrite.close()
 
 output_data = []
 
 for output in outputs:
-    output_data.append(labels2idx[output])
+    output_data.append(label2idx[output])
 
 output_data = to_categorical(output_data, len(output_data))
 
@@ -67,12 +65,26 @@ output_data = to_categorical(output_data, len(output_data))
 print(output_data[0])
 
 model = Sequential()
-model.add(Embedding(len(chars), 64))
-model.add(LSTM(128, return_sequences=True))
+model.add(LSTM(128))
 model.add(Dense(len(output_data), activation='softmax'))
-model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['acc'])
 
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
 
-model.fit(input_data, output_data, epochs=16)
+model.fit(input_data, output_data, epochs=128)
+
+# Salvar model
+model.save('model.h5')
+
+# Classificar texto em um entidade
+def classify(text):
+    # Criar um array de entrada
+    x = np.zeros((1, 48, 256), dtype='float32')
+
+    # Preencher o array com dados do texto.
+    for k, ch in enumerate(bytes(text.encode('utf-8'))):
+        x[0, k, int(ch)] = 1.0
+
+    # Fazer a previsão
+    out = model.predict(x)
+    idx = out.argmax()
+    print(idx2label[idx])
